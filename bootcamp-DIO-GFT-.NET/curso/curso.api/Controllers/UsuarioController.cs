3ee536b7f89ab727b08dd3a93c36d19617e18f07
michelmsnascimento <michelmsnascimento@gmail.com>
2022-02-06 23:59:35 -0300

    atualizacao do projeto curso

---
+++ bootcamp-DIO-GFT-.NET/curso/curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
