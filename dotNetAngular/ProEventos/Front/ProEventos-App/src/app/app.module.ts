a7db3714dc576050beb273585950016cbf432258
michelmsnascimento <michelmsnascimento@gmail.com>
2022-07-16 18:08:07 -0300

    Seção 5: Angular, interpolação diretiva e binding

--- dotNetAngular/ProEventos/Front/ProEventos-App/src/app/app.module.ts
+++ dotNetAngular/ProEventos/Front/ProEventos-App/src/app/app.module.ts
@@ -7,18 +7,28 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
-  ],
+    PalestrantesComponent,
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    CollapseModule.forRoot(),
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
